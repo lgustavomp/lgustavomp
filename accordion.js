4497bb87b4372125a465e689380c832cf488cc50
lgustavomp <lgustavomp@hotmail.com>
2024-01-29 17:10:22 -0300

    01_29)24

---
+++ accordion.js
@@ -0,0 +1,22 @@
+const accordion = document.getElementsByClassName('content_box');
+// const arrow = document.getElementsByClassName('ma')
+for (i = 0; i<accordion.length; i++ ){
+    accordion[i].addEventListener('click', function(){
+        // accordion.classList.remove('active');
+        // console.log(this.parentNode.childNodes);
+        this.parentNode.childNodes.forEach(element => {
+            // element.classList.remove('active');
+ 
+            if (element.nodeName === 'DIV') {
+                // console.log(element); 
+                element.classList.remove('active')
+                // this.parentNode.childNodes.classList.remove('open');
+                element.childNodes[1].childNodes[3].classList.remove('open')
+            }
+
+        });
+        this.classList.toggle('active');
+        this.childNodes[1].childNodes[3].classList.toggle('open');
+        
+
+    })}
