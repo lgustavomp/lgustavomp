abb273b19733c2dfaccfd1b2c5301cf2f6733577
lgustavomp <lgustavomp@hotmail.com>
2024-02-02 15:57:03 -0300

    02_02_24

--- tags.js
+++ tags.js
@@ -1,56 +1,55 @@
-// event listener no click -> identificar classe .hashtag clicada
-
-// $(".hashtag").on("click", function () {
-//     const hashtagParent = document.getElementsByClassName("hashtag");
-//     const hashtagChild = hashtagParent;
-
-//     console.log(hashtagChild)
-// });
-
 const hashtagParent = document.getElementsByClassName('hashtag');
+const etiquetaParent = document.getElementsByClassName('etiqueta');
+const etiquetaTag = document.getElementsByClassName('etiqueta_tag');
+let tagResult = false;
 
 
 for (i = 0; i<hashtagParent.length; i++ ){
     hashtagParent[i].addEventListener('click', function(){
         hashtagChild = this.textContent;
-        // console.log(hashtagParent);
+        
+        Array.from(etiquetaTag).forEach(element => {
+            let arrayTags = element.textContent;
+            console.log(arrayTags);
+        })
 
             Array.from(hashtagParent).forEach(element => {
                 // console.log(element);
                 element.classList.remove('vermelho');
             });
+            Array.from(etiquetaParent).forEach(element => {
+                element.classList.remove('vermelha')
+            });
 
+        function tagChecker(hashtagChild,arrayTags) {
+            for (const key in hashtagChild) {
+                if(hashtagChild[key] === arrayTags[key]) tagResult=true
+            }
+            tagResult=false
+        }
+        console.log(tagResult);
         console.log(hashtagChild);
+        console.log(tagChecker(hashtagChild,arrayTags));
         this.classList.add('vermelho');
-
-
-        // this.childNodes.forEach (element => {
-        //     if (element.nodeName === 'DIV') {
-        //         element.classList.remove('vermelho')
-        //         console.log(element.nodeName)
-        //         } else {
-        //             this.classList.toggle('vermelho')
-        //         }
-        // })
-
-        // this.childNodes.forEach(element => {
- 
-        //     if (element.nodeName === 'DIV') {
-        //         element.classList.remove('active')
-        //         element.childNodes[1].childNodes[3].classList.remove('open')
-        //     }
-
-        // });
-        // this.classList.toggle('active');
-        // this.childNodes[1].childNodes[3].classList.toggle('open');
-        
-
-        
     })}
 
 
-// buscar .etiqueta_tag com string compativel com .hashtag
+//console.log(etiquetaParent);
 
+
+// buscar .etiqueta_tag com string compativel com .hashtag
+// children-> div.etiqueta_descricao-> children -> spam.etiqueta_tag
+
+// for (i = 0; i<etiquetaParent.length; i++ ){
+//     etiquetaParent[i].addEventListener('click', function(){
+
+//         const etiquetaTag = this.getElementsByClassName('etiqueta_tag');
+//         Array.from(etiquetaTag).forEach(element => {
+//             console.log(element.textContent);
+//         })
+        
+//     })
+// }
 
 
 
